b02c5af88b17048092d86031cc15b9628c94c8cb
Stephan Hoyer <ste.hoyer@gmail.com>
2012-04-26 15:09:35 +0200

    Remove jQuery as dependency

--- js/aoestatic/common.js
+++ js/aoestatic/common.js
@@ -1,58 +1,78 @@
-/**
- * Send ajax request to the Magento store in order to insert dynamic content into the
- * static page delivered from Varnish
- *
- * @author Fabrizio Branca
- */
-$.noConflict();
-jQuery(document).ready(function($) {
+var isJQ = (typeof jQuery !== 'undefined');
+if (isJQ) $.noConflict();
+(function() {
+  if (typeof AJAXHOME_URL !== 'undefined') {
+    return;
+  }
 
-	var data = { 
-		getBlocks: {}
-	};
+  /**
+   * Send ajax request to the Magento store in order to insert dynamic content into the
+   * static page delivered from Varnish
+   */
+  var updateBlocks;
+  var callHome;
 
-	// add placeholders
-	var counter = 0;
-	$('.placeholder').each(function() {
-		var id = $(this).attr('id');
-		if (!id) {
-			// create dynamic id
-			id = 'ph_' + counter;
-			$(this).attr('id', id);
-		}
-		var rel = $(this).attr('rel');
-		if (rel) {
-			data.getBlocks[id] = rel;
-			counter++;
-		} else {
-			throw 'Found placeholder without rel attribute';
-		}
-	});
+  var placeholderElems = function placeholderElems() {
+    return isJQ ? jQuery('.placeholder') : $$('.placeholder');
+  }
 
-	// add current product
-	if (typeof CURRENTPRODUCTID !== 'undefined' && CURRENTPRODUCTID) {
-		data.currentProductId = CURRENTPRODUCTID;
-	}
+  callHome = function callHome() {
+    var data = {getBlocks: {}};
+    var counter = 0;
+    var elems = placeholderElems();
 
-	// E.T. phone home
-    if (
-        (typeof data.currentProductId !== 'undefined' || counter > 0) && 
-        typeof AJAXHOME_URL !== 'undefined'
-    ) {
-		$.get(
-			AJAXHOME_URL,
-			data,
-			function (response) {
-				for(var id in response.blocks) {
-					$('#' + id).html(response.blocks[id]);
-				}
-				// inject session if (TODO: check if this is really needed)
-				// $.cookie('frontend', response.sid, { path: '/' });
-				
-				// TODO: trigger event
-			},
-			'json'
-		);
-	}
-	
-});
+    elems.each(function(el) {
+      el = isJQ ? jQuery(this) : $(el);
+      var id = isJQ ? el.attr('id') : el.readAttribute('id');
+      if (!id) {
+        // create dynamic id
+        id = 'ph_' + counter;
+        if (isJQ) {
+          el.attr('id', id)
+        } else {
+          el.writeAttribute('id', id);
+        }
+      }
+      var rel = isJQ ? el.attr('rel') : el.readAttribute('rel');
+      if (rel) {
+        data.getBlocks[id] = rel;
+        counter++;
+      }
+    });
+
+    // add current product
+    if (typeof CURRENTPRODUCTID !== 'undefined' && CURRENTPRODUCTID) {
+      data.currentProductId = CURRENTPRODUCTID;
+    }
+
+    if (typeof data.currentProductId !== 'undefined' || counter > 0) {
+      if (isJQ) {
+        jQuery.get(AJAXHOME_URL, data, updateBlocks, 'json');
+      } else {
+        $H(data.getBlocks).each(function(block) {
+          data['getBlocks[' + block[0] + ']'] = block[1];
+        });
+        new Ajax.Request(AJAXHOME_URL, {
+          method: 'get',
+          parameters: data,
+          onSuccess: updateBlocks 
+        });
+      }
+    }
+  }
+  updateBlocks = function updateBlocks(response) {
+    response = isJQ ? response : response.transport.responseText.evalJSON();
+    for(var id in response.blocks) {
+      if (isJQ) {
+        jQuery('#' + id).html(response.blocks[id]);
+      } else {
+        $(id).update(response.blocks[id]);
+      }
+    }
+  }
+  if (isJQ) {
+    jQuery(document).ready(callHome);
+  } else {
+    document.observe("dom:loaded", callHome);
+  }  
+}());
